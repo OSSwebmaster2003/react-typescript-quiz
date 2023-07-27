6b0ad71bfd1a9956fc9d716f696fd5cee9fa34f6
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-07-27 02:40:39 -0700

    utils and api files finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+import { Difficulty } from './API';
 import QuestionCard from './components/QuestionCard';
 
 const TOTAL_QUESTIONS = 10;
@@ -15,20 +17,22 @@ function App() {
   const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {}
 
   const nextQuestion = () => {}
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS , Difficulty.EASY));
+  
   return (
     <div className="App">
       <h1>React Quiz</h1>
       <button className='start' onClick={startTrivia}>Start</button>
       <p className="score">Score : 0</p>
       <p>Loading Question...</p>
-      <QuestionCard 
-      questionNumber={number + 1}
-      totalQuestions={TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number] : undefined}
-      callback={checkAnswer}
-      />
+      {/* <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className='next' onClick={nextQuestion}>NextQuestion</button>
     </div>
   );
