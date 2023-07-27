0da8364431db16d5b654ce2e57cd0148b07a1fac
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-07-27 02:07:59 -0700

    question card logic created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+  const startTrivia = async () => {}
+
+  const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {}
+
+  const nextQuestion = () => {}
   return (
     <div className="App">
-      Quiz
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>Start</button>
+      <p className="score">Score : 0</p>
+      <p>Loading Question...</p>
+      <QuestionCard/>
+      <button className='next' onClick={nextQuestion}>NextQuestion</button>
     </div>
   );
 }
