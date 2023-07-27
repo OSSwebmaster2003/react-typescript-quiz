4a7f9a37b1f417cdc73c12477ca5a8b9168c04e5
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-07-27 07:00:45 -0700

    next question finished

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,15 @@ function App() {
     }
   }
 
-  const nextQuestion = () => {}
+  const nextQuestion = () => {
+    // move to next question if not the last question
+    const nextQuestion = number + 1
+    if(nextQuestion === TOTAL_QUESTIONS ){
+      setGameOver(true)
+    }else{
+      setNumber(nextQuestion)
+    }
+  }
   
   return (
     <div className="App">
